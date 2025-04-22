851f22e008b65b517aeba245a43481a0eadfe490
NicHub <nicolas.jeanmonod@gmail.com>
2025-04-22 12:36:14 +0200

    works with 3 GPIOs

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,21 @@
 #define REED_2 GPIO_NUM_2
 #define REED_3 GPIO_NUM_4
 
+void readReed()
+{
+    int curVal = digitalRead(REED_3) * 100 + digitalRead(REED_2) * 10 + digitalRead(REED_1);
+    static int lastVal = -1;
+    if (lastVal == curVal)
+        return;
+    lastVal = curVal;
+
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.begin(BAUD_RATE);
+    Serial.print("\nGPIO : ");
+    Serial.printf("%03d", curVal);
+    digitalWrite(LED_BUILTIN, HIGH);
+}
+
 void setup()
 {
     pinMode(REED_1, INPUT);
@@ -20,13 +35,7 @@ void setup()
     uint64_t mask = (1ULL << REED_1) | (1ULL << REED_2) | (1ULL << REED_3);
     esp_sleep_enable_ext1_wakeup(mask, ESP_EXT1_WAKEUP_ANY_HIGH);
 
-    digitalWrite(LED_BUILTIN, LOW);
-    Serial.begin(BAUD_RATE);
-    Serial.print("GPIO : ");
-    Serial.print(digitalRead(REED_1));
-    Serial.print(digitalRead(REED_3));
-    Serial.println(digitalRead(REED_2));
-    digitalWrite(LED_BUILTIN, HIGH);
+    readReed();
     delay(1000); // debounce
 
     esp_deep_sleep_start();
@@ -35,4 +44,5 @@ void setup()
 void loop()
 {
     yield();
+    readReed();
 }
