444acce75d897d37379a90cf89699cc6617a5df5
NicHub <nicolas.jeanmonod@gmail.com>
2026-03-15 13:33:51 +0100

    Refine MQTT and NTFY configuration guards

--- src/user_settings-examples/user_settings.h
+++ src/user_settings-examples/user_settings.h
@@ -27,7 +27,7 @@
 #define LORA_BW 62.5
 #define LORA_SF 12
 #define LORA_CR 8
-#define LORA_SYNCWORD 0x12
+#define LORA_SYNCWORD RADIOLIB_SX126X_SYNC_WORD_PRIVATE
 #define LORA_POWER 14
 #define LORA_PREAMBLELENGTH 12
 #define LORA_TCXOVOLTAGE 1.6
@@ -38,22 +38,18 @@
 #define MQTT_SERVER "mqtt.ouilogique.ch"
 #define MQTT_USE_TLS true
 #define MQTT_PORT (MQTT_USE_TLS ? 8883 : 1883)
-#define MQTT_TOPIC "mbx_nj"
-#define MQTT_TOPIC_HEARTBEAT_RX "mbx_nj/heartbeat_rx"
-#define MQTT_TOPIC_WAKEUP_PIN_HIGH "mbx_nj/pin"
-#define MQTT_TOPIC_HEARTBEAT_TX "mbx_nj/heartbeat"
-#define MQTT_TOPIC_BOOT "mbx_nj/boot"
-#define MQTT_TOPIC_LOW_BATTERY "mbx_nj/low_battery"
+#define MQTT_TOPIC "mbx_rz"
+#define MQTT_TOPIC_HEARTBEAT_RX "mbx_rz/heartbeat_rx"
+#define MQTT_TOPIC_WAKEUP_PIN_HIGH "mbx_rz/pin"
+#define MQTT_TOPIC_HEARTBEAT_TX "mbx_rz/heartbeat"
+#define MQTT_TOPIC_BOOT "mbx_rz/boot"
 
 /** @brief NTFY settings. */
 #define NTFY_ENABLED true
 #define NTFY_INCLUDE_JSONL false
 #define NTFY_NOTIFY_HEARTBEAT_TX true
 #define NTFY_TITLE_PIN_HIGH "📩"
-#define NTFY_TITLE_LOW_BATTERY "🪫"
-#define NTFY_TITLE_PIN_HIGH_LOW_BATTERY "📩 + 🪫"
 #define NTFY_TITLE_HEARTBEAT_TX "🔔"
-#define NTFY_TITLE_HEARTBEAT_TX_LOW_BATTERY "🔔 + 🪫"
 #define NTFY_SERVER "https://ntfy.ouilogique.ch/"
-#define NTFY_RECIPIENT_NAME "nico"
-#define NTFY_TOPIC "mbx_nj"
+#define NTFY_RECIPIENT_NAME "rolf"
+#define NTFY_TOPIC "mbx_rz"
